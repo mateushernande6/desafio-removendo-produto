7f7798bd785d572ebb5868dcdb10c9611ea43d40
Mateus Hernandes <mateushernandes939@gmail.com>
2023-11-08 21:18:54 -0300

    fix: fixed question

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,6 @@ import { listaDoCarrinho } from "./database.js";
 // Implemente a função removeDoCarrinho que recebe um produto como parâmetro e remove esse produto do carrinho.
 export function removeDoCarrinho(produto) {
   /* desenvolva sua lógica aqui */
-  // const removed  = listaDoCarrinho
 
   // Certifique-se de que o produto é removido do carrinho usando o método splice.
   // Lembre-se de que você deve retornar o produto removido no return da função.
